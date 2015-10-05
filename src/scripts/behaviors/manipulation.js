05f2d3407b833af730df67e152aab62f55135b33
Simon <simonxd@gmail.com>
2015-10-05 10:19:47 -0300

    element matches

--- src/scripts/behaviors/manipulation.js
+++ src/scripts/behaviors/manipulation.js
@@ -16,3 +16,24 @@ exports.replaceInnerHTML = function (element, contents) {
 
     Polymer.dom(element).innerHTML = contents;
 };
+
+exports.applyStyle = function (elements, property, value) {
+
+    // convert elements into an elements array
+    if (typeof elements === 'string') {
+        // elements is a CSSSelector
+        elements = document.querySelectorAll(elements);
+
+        // convert into array
+        elements = Array.prototype.slice.call(elements, 0);
+
+    } else if (!_.isArray(elements)) {
+        // probably NodeList
+        // https://developer.mozilla.org/en/docs/Web/API/NodeList
+        elements = Array.prototype.slice.call(elements, 0);
+    }
+
+    elements.forEach(function (el) {
+        el.style[property] = value;
+    });
+};
